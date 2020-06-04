70a6500c577c368a428bda1cd8e04cce489264bb
Olga Ivanova <olga.iva@gmail.com>
2020-06-04 15:13:52 +0200

    update

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
-const cookieParser = require("cookie-parser");
+var cookieSession = require("cookie-session");
+// const cookieParser = require("cookie-parser");
+
+// app.use(
+//     cookieSession({
+//         secret: secret,
+//         maxAge: 1000 * 60 * 60 * 24 * 14,
+//     })
+// );
 
 /// setting up Handelbars as Express template engine:
 const hb = require("express-handlebars");
@@ -20,38 +28,39 @@ app.use(express.static("./public"));
 
 ///
 
-app.use((req, res, next) => {
-    console.log("middleware running");
-    console.log(`ran ${req.method} at ${req.url} route`);
-    if (!req.cookies.petSigned) {
-        res.redirect("/petition");
-    } else {
-        next();
-    }
-});
-
-app.use((req, res, next) => {
-    console.log("middleware running");
-    console.log(`ran ${req.method} at ${req.url} route`);
-    if (!req.cookies.petSigned && req.url != "/petition") {
-        res.redirect("/petition");
-    } else {
-        next();
-    }
-});
+// app.use((req, res, next) => {
+//     console.log("middleware running");
+//     console.log(`ran ${req.method} at ${req.url} route`);
+//     if (!req.cookies.petSigned && req.url != "/petition") {
+//         res.redirect("/petition");
+//     } else {
+//         next();
+//     }
+// });
 
 app.get("/petition", (req, res) => {
     res.render("petition");
 });
 
 app.post("/petition", (req, res) => {
-    res.cookie("petSigned", true);
+    // res.cookie("petSigned", true);
     db.addName(req.body.first, req.body.last, req.body.signature)
         .then(() => {
+            console.log("new record added");
             res.redirect("/signed");
         })
         .catch((err) => {
             console.log("error in addName:", err);
+        });
+});
+
+app.get("/signed", (req, res) => {
+    db.getCount()
+        .then((results) => {
+            res.render("signed", results);
+        })
+        .catch((err) => {
+            console.log("err in getCount:", err);
         });
 });
 
