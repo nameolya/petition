813f95cbbc3e5e23843c32c98420d22baaa196ab
Olga Ivanova <olga.iva@gmail.com>
2020-06-05 12:18:33 +0200

    cookie-session added

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ var db = spicedPg("postgres:postgres:postgres@localhost:5432/signatures");
 
 module.exports.addName = (first, last, signature) => {
     return db.query(
-        `INSERT INTO signatures (first, last, signature) VALUES ($1, $2, $3)`,
+        `INSERT INTO signatures (first, last, signature) VALUES ($1, $2, $3) RETURNING id`,
         [first, last, signature]
     );
 };
--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const express = require("express");
 const app = express();
 const db = require("./db");
 var cookieSession = require("cookie-session");
-// const cookieParser = require("cookie-parser");
+const { secret } = require("./secrets.json");
 
-// app.use(
-//     cookieSession({
-//         secret: "here goes the secret",
-//         maxAge: 1000 * 60 * 60 * 24 * 14,
-//     })
-// );
+//
+app.use(
+    cookieSession({
+        secret: secret,
+        maxAge: 1000 * 60 * 60 * 24 * 14,
+    })
+);
 
 /// setting up Handelbars as Express template engine:
 const hb = require("express-handlebars");
@@ -26,19 +27,10 @@ app.use(
 ///
 app.use(express.static("./public"));
 
-///
-
-// app.use((req, res, next) => {
-//     console.log("middleware running");
-//     console.log(`ran ${req.method} at ${req.url} route`);
-//     if (!req.cookies.petSigned && req.url != "/petition") {
-//         res.redirect("/petition");
-//     } else {
-//         next();
-//     }
-// });
+//______
 
 app.get("/", (req, res) => {
+    console.log("req.session:", req.session);
     res.redirect("/petition");
 });
 
@@ -47,47 +39,65 @@ app.get("/petition", (req, res) => {
 });
 
 app.post("/petition", (req, res) => {
-    // res.cookie("petSigned", true);
-    db.addName(req.body.first, req.body.last, req.body.signature)
-        .then(() => {
-            console.log("new record added");
-            res.redirect("/signed");
-        })
-        .catch((err) => {
-            console.log("error in addName:", err);
-        });
+    if ((req.body.first, req.body.last, req.body.signature)) {
+        db.addName(req.body.first, req.body.last, req.body.signature)
+            .then(() => {
+                console.log("first, last:", req.body.first, req.body.last);
+                console.log("new record added");
+                req.session.sigID = response.rows[0].id;
+                console.log("req.session.sigID:", req.session.sigID);
+                res.redirect("/signed");
+            })
+            .catch((err) => {
+                console.log("error in addName:", err);
+            });
+    } else {
+        res.redirect("/petition");
+        alert("please fill in all the required fields");
+    }
 });
 
 app.get("/signed", (req, res) => {
-    db.getCount()
-        .then((results) => {
-            console.log("Total Signed:", results);
-            res.render("signed", {
-                count: results.rows[0].count,
+    if (req.session.sigID) {
+        db.getCount()
+            .then((results) => {
+                console.log("Total Signed:", results);
+                res.render("signed", {
+                    count: results.rows[0].count,
+                });
+            })
+            .catch((err) => {
+                console.log("err in getCount:", err);
             });
-        })
-        .catch((err) => {
-            console.log("err in getCount:", err);
-        });
+    } else {
+        console.log("/signed: out of cookie-session redirect");
+        res.redirect("/petition");
+    }
 });
 
 app.get("/signers", (req, res) => {
-    db.getNames()
-        .then((results) => {
-            console.log("List of persons signed:", results);
-            let signers = [];
-            for (let i = 0; i < results.rows.length; i++) {
-                let fullName = " ";
-                fullName += results.rows[i].first + " " + results.rows[i].last;
-                signers.push(fullName);
-            }
-            res.render("signers", {
-                signers: signers,
+    if (req.session.sigID) {
+        db.getNames()
+            .then((results) => {
+                console.log("List of persons signed:", results);
+                let signers = [];
+                for (let i = 0; i < results.rows.length; i++) {
+                    let fullName = " ";
+                    fullName +=
+                        results.rows[i].first + " " + results.rows[i].last;
+                    signers.push(fullName);
+                }
+                res.render("signers", {
+                    signers: signers,
+                });
+            })
+            .catch((err) => {
+                console.log("err in getNames:", err);
             });
-        })
-        .catch((err) => {
-            console.log("err in getNames:", err);
-        });
+    } else {
+        console.log("/signers: out of cookie-session redirect");
+        res.redirect("/petition");
+    }
 });
 
 app.listen(8080, () => console.log("server is listening..."));
